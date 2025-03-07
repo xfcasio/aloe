ab85e070453610477dad3f634723d6c77b5aa389
xfcisco <xfc1@pm.me>
2025-03-07 02:02:56 +0200

    prepend `aloe_` prefex to printf to avoid name collisions

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,14 @@ struct AloeAllocator(System, AtomicUsize);
 #[global_allocator]
 static GLOBAL: AloeAllocator = AloeAllocator(System, AtomicUsize::new(0));
 
-use printf as c_printf;
+use printf as aloe_c_printf;
 extern "C" {
     fn printf(format: *const u8, ...) -> i32;
 }
 
-macro_rules! printf {
+macro_rules! aloe_printf {
     ($s: expr $(, $args: expr)*) => (
-        { c_printf(concat!($s, "\0").as_ptr() $(, $args)*); }
+        { aloe_c_printf(concat!($s, "\0").as_ptr() $(, $args)*); }
     )
 }
 
@@ -22,8 +22,8 @@ unsafe impl GlobalAlloc for AloeAllocator {
         self.1.fetch_add(1, Ordering::SeqCst);
 
         let alloc_num = self.1.load(Ordering::Relaxed) - 1;
-        (0..alloc_num).for_each(|_| printf!("  "));
-        printf!(
+        (0..alloc_num).for_each(|_| aloe_printf!("  "));
+        aloe_printf!(
             "<\u{001b}[33mallocation \u{001b}[32mdepth\u{001b}[37m=\u{001b}[32m%d\u{001b}[0m>\n",
             alloc_num);
         
@@ -32,8 +32,8 @@ unsafe impl GlobalAlloc for AloeAllocator {
 
     unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
         let alloc_num = self.1.load(Ordering::Relaxed) - 1;
-        (0..alloc_num).for_each(|_| printf!("  "));
-        printf!(
+        (0..alloc_num).for_each(|_| aloe_printf!("  "));
+        aloe_printf!(
             "</\u{001b}[33mallocation \u{001b}[32mdepth\u{001b}[37m=\u{001b}[32m%d\u{001b}[0m>\n",
             alloc_num);
 
